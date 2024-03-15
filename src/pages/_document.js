710ccc4d65c2b565e064efeb7d3809e0ac41adc1
RyoOshimaPropo <ryo.oshima@propo.fm>
2024-03-16 01:23:43 +0900

    init push

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="ja">
+      <Head>
+        <meta name="viewport" content="initial-scale=1" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
