151241dfcd738d5f6ae53b1c6b08453883078abd
ryoohshima <chlslmps@gmail.com>
2024-03-17 16:34:11 +0900

    headerの実装
    - アイコン追加
    - _app.jsでヘッダー読み込み

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,25 @@
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
 import Navigation from '@/components/organisms/navigation';
 
 const App = ({ Component, pageProps }) => {
   return (
     <>
-      <header>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          boxShadow: 1,
+          py: 1,
+          px: 3,
+        }}
+      >
+        <Link href="/">
+          <img src="/img/icon.png" alt="" width={40} height={40} />
+        </Link>
         <Navigation />
-      </header>
+      </Box>
       <Component {...pageProps} />
     </>
   );
