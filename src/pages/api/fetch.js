880d2c80462b50d64c1004a8c6ba5cffaecce01b
ryoohshima <ryo.oshima@nijibox.co.jp>
2024-03-25 15:42:18 +0900

    fetch用のAPIを作成
    - add: /pages/api/fetchの作成
    - fix: insertの関数名をfetchに併せて変更

--- src/pages/api/fetch.js
+++ src/pages/api/fetch.js
@@ -1,15 +1,14 @@
-import { supabase } from "@/lib/supabase";
+import { supabase, fetchData } from "@/lib/supabase";
 
 /**
- * supabaseにデータを追加する関数
- * @function insert
+ * supabaseからデータを取得する関数
+ * @function fetch
  * @param {Object} req - The request object
  * @param {string} req.tableName - The table name
- * @param {Object} req.data - The data to insert
  * @param {Object} res - The response object
  * @returns {Object} - The response object
  */
-const insert = async (req, res) => {
+const fetch = async (req, res) => {
   // POST以外のリクエストは受け付けない
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
@@ -17,18 +16,12 @@ const insert = async (req, res) => {
 
   // リクエストボディからデータを取得
   const tableName = req.body.tableName;
-  const insertData = req.body.data;
 
   // supabaseにデータを追加
-  const { data, error } = await supabase.from(tableName).insert(insertData).select();
-
-  // エラーがあればエラーメッセージを返す
-  if (error) {
-    return res.status(500).json({ message: error.message });
-  }
+  const data = await fetchData(supabase, tableName, '*');
 
   // データを返す
   return res.status(200).json(data);
 }
 
-export default insert;
+export default fetch;
